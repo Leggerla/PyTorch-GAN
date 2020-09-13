c1fd30f155fe6e674c3e808e2afdba87383cfb1d
Lucy Airapetyan <52498629+Leggerla@users.noreply.github.com>
2020-09-13 05:37:08 +0300

    Update relativistic_gan.py

--- implementations/relativistic_gan/relativistic_gan.py
+++ implementations/relativistic_gan/relativistic_gan.py
@@ -222,7 +222,7 @@ for epoch in range(opt.n_epochs):
         sum_g_loss.append(g_loss.item())
 
         autocorr = torch.mean(autocorrelation(gen_imgs.data, dim=1))
-        print (autocorr)
+        print (gen_imgs.data)
         print (torch.mean(autocorrelation(gen_imgs.data, dim=0)))
         if autocorr > best_autocorrelation:
           print (autocorr)
