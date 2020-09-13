cdab28d5f4927870a1732efba1dfc7ddf43a6744
Lucy Airapetyan <52498629+Leggerla@users.noreply.github.com>
2020-09-13 05:28:55 +0300

    Update relativistic_gan.py

--- implementations/relativistic_gan/relativistic_gan.py
+++ implementations/relativistic_gan/relativistic_gan.py
@@ -221,8 +221,8 @@ for epoch in range(opt.n_epochs):
         sum_d_fake_loss.append(fake_loss.item())
         sum_g_loss.append(g_loss.item())
 
-        autocorrelation = torch.sum(autocorrelation(gen_imgs, dim=1))
-        if autocorrelation > best_autocorrelation:
+        autocorr = torch.sum(autocorrelation(gen_imgs, dim=1))
+        if autocorr > best_autocorrelation:
           batches_done = epoch * len(dataloader) + i
           torch.save(real_imgs.data, "charts/real_%d.pt" % batches_done)
           torch.save(gen_imgs.data, "charts/gen_%d.pt" % batches_done)
