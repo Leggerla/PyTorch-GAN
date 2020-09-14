36bfc4a8a3cae18e46a19e538d0e71c647dd5ca4
Lucy Airapetyan <52498629+Leggerla@users.noreply.github.com>
2020-09-14 19:50:40 +0300

    Update relativistic_gan.py

--- implementations/relativistic_gan/relativistic_gan.py
+++ implementations/relativistic_gan/relativistic_gan.py
@@ -170,7 +170,6 @@ for epoch in range(opt.n_epochs):
 		fake = Variable(Tensor(imgs.shape[0], 1).fill_(0.0), requires_grad=False)
 
 		# Configure input
-		print('Original', imgs.shape)
 		real_imgs = Variable(imgs.type(Tensor))[:, None, :]
 
 		# -----------------
