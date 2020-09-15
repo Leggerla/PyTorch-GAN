b11883ec37401df382b696c8ba988ad41fd3664e
Lucy Airapetyan <52498629+Leggerla@users.noreply.github.com>
2020-09-15 19:16:16 +0300

    Update relativistic_gan.py

--- implementations/relativistic_gan/relativistic_gan.py
+++ implementations/relativistic_gan/relativistic_gan.py
@@ -236,7 +236,7 @@ for epoch in range(opt.n_epochs):
 	autocorr = torch.sum(mask).float()
 	if autocorr >= best_autocorrelation:
 		#if autocorr > best_autocorrelation:
-		print('Std', epoch, best_autocorrelation)
+		#print('Std', epoch, best_autocorrelation)
 		best_autocorrelation = autocorr.item()
 		torch.save(real_associate.data, "charts/real.pt")
 		torch.save(gen_associate.data, "charts/gen.pt")
