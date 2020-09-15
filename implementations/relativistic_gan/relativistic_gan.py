295c4a6f25c9938163c2ccbeba2716e599d52164
Lucy Airapetyan <52498629+Leggerla@users.noreply.github.com>
2020-09-15 19:28:56 +0300

    Update relativistic_gan.py

--- implementations/relativistic_gan/relativistic_gan.py
+++ implementations/relativistic_gan/relativistic_gan.py
@@ -74,7 +74,7 @@ class StockDataset(torch.utils.data.Dataset):
 	    if enum - i < window:
 	      break
 	    res.append(array[i:i + roll][:, 0])
-	  return (torch.stack(res)-min+1e-8)/(max-min+1e-8)
+	  return 2*(torch.stack(res)-min+1e-8)/(max-min+1e-8)-1
 
 
 class Generator(nn.Module):
