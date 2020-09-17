ed6558de6ca49a1691ad26b9b059adef45223bc4
Lucy Airapetyan <52498629+Leggerla@users.noreply.github.com>
2020-09-17 21:23:05 +0300

    Update relativistic_gan.py

--- implementations/relativistic_gan/relativistic_gan.py
+++ implementations/relativistic_gan/relativistic_gan.py
@@ -243,8 +243,8 @@ for epoch in range(opt.n_epochs):
 
 			print (real_pred)
 			print (valid.long())
-			real_loss = crossentropy_loss(real_pred, valid.long())
-			fake_loss = crossentropy_loss(fake_pred, fake.long())
+			real_loss = adversarial_loss(real_pred, valid.long())
+			fake_loss = adversarial_loss(fake_pred, fake.long())
 
 			d_loss.backward()
 			optimizer_D.step()
