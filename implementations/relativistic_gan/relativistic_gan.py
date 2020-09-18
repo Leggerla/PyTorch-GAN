4d3e0af94ab966fef0c1a93a85a93e859793c489
Lucy Airapetyan <52498629+Leggerla@users.noreply.github.com>
2020-09-18 03:58:38 +0300

    Update relativistic_gan.py

--- implementations/relativistic_gan/relativistic_gan.py
+++ implementations/relativistic_gan/relativistic_gan.py
@@ -69,8 +69,7 @@ class StockDataset(torch.utils.data.Dataset):
 
 	def rolling_periods(self, start_points, window, roll):
 		base = []
-		associate = [] 
-		print (self.associate_timeseries)
+		associate = []
 		max = torch.max(self.associate_timeseries)
 		min = torch.min(self.associate_timeseries)
 		enum = self.associate_timeseries.shape[0]
