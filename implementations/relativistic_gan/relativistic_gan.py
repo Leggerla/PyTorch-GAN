addb6c2f7a8465ea4a6f694b9e082c14b71c08f8
Lucy Airapetyan <52498629+Leggerla@users.noreply.github.com>
2020-09-28 13:35:05 +0300

    Update relativistic_gan.py

--- implementations/relativistic_gan/relativistic_gan.py
+++ implementations/relativistic_gan/relativistic_gan.py
@@ -91,7 +91,7 @@ class StockDataset(torch.utils.data.Dataset):
 			associate.append(self.associate_timeseries[i:i + window+1])
 			spy.append(spy_prices[i:i + window+1])
 			vix_open.append(start_points[i:i + window+1])
-			indices.append(torch.arrange(i, i+window+1))
+			indices.append(torch.arange(i, i+window+1))
 		if opt.OHLC:
 			window = 4*(window+1)
 			roll = 4*(roll+1)
