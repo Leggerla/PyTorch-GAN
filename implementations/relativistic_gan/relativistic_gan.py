e1a8b0ef73ed80bab4433b45bfe79dcdb3db8a78
Lucy Airapetyan <52498629+Leggerla@users.noreply.github.com>
2020-10-15 02:56:53 +0300

    Update relativistic_gan.py

--- implementations/relativistic_gan/relativistic_gan.py
+++ implementations/relativistic_gan/relativistic_gan.py
@@ -111,12 +111,13 @@ class Generator(nn.Module):
 	def __init__(self):
 		super(Generator, self).__init__()
 		
-		def generator_block(in_filters, out_filters, kernel_size=(2, 5), padding=(2, 2, 1, 0)):
+		last_pad = 1 if opt.channels == 1 else 0
+		
+		def generator_block(in_filters, out_filters, kernel_size=(2, 5), padding=(2, 2, last_pad, 0)):
 			block = [nn.ZeroPad2d(padding), nn.Conv2d(in_filters, out_filters, kernel_size=kernel_size),
 				 nn.BatchNorm2d(out_filters),nn.LeakyReLU(0.2, inplace=True)]
 			return block
 
-		last_pad = 1 if opt.channels == 1 else 0
 		self.model = nn.Sequential(
 			*generator_block(1, 512),
 			*generator_block(512, 256),
@@ -125,7 +126,7 @@ class Generator(nn.Module):
 			*generator_block(64, 32),
 			*generator_block(32, 16),
 			*generator_block(16, 8),
-			*generator_block(8, 4, kernel_size=(3, 3), padding=(1, 1, 1, 1)),
+			*generator_block(8, 4, kernel_size=(3, 3), padding=(1, 1, last_pad, 1)),
 			*generator_block(4, 2, kernel_size=(3, 3), padding=(1, 1, last_pad, 1)),
 			*generator_block(2, 1, kernel_size=(2, 1), padding=(0, 0, 0, 0)))
 		if opt.OHLC == True:
