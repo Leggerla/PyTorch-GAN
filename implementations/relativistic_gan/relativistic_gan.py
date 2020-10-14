576ca4091732eaae13f5e28d4b2cdadae219386d
Lucy Airapetyan <52498629+Leggerla@users.noreply.github.com>
2020-10-15 02:40:46 +0300

    Update relativistic_gan.py

--- implementations/relativistic_gan/relativistic_gan.py
+++ implementations/relativistic_gan/relativistic_gan.py
@@ -170,7 +170,7 @@ class Discriminator(nn.Module):
 
 	def forward(self, base, associate):
 		print (base.shape, associate.shape)
-		out = torch.cat([base, associate], dim=1)
+		out = torch.cat([base, associate], dim=-1)
 		if opt.channels == 1:
 			out = out[:, None, :]
 		print (out.shape)
