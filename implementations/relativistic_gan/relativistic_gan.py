7a62dda7ef6f94bde6affe8d078eff87ceb78cbc
Lucy Airapetyan <52498629+Leggerla@users.noreply.github.com>
2020-11-20 21:56:48 +0300

    Update relativistic_gan.py

--- implementations/relativistic_gan/relativistic_gan.py
+++ implementations/relativistic_gan/relativistic_gan.py
@@ -111,8 +111,8 @@ class Generator(nn.Module):
 	def __init__(self):
 		super(Generator, self).__init__()
 		
-		def generator_block(in_filters, out_filters, kernel_size=(2, 5), padding=(2, 2, 1, 0), groups=in_filters):
-			block = [nn.ZeroPad2d(padding), nn.Conv2d(in_filters, out_filters, kernel_size=kernel_size),
+		def generator_block(in_filters, out_filters, kernel_size=(2, 5), padding=(2, 2, 1, 0)):
+			block = [nn.ZeroPad2d(padding), nn.Conv2d(in_filters, out_filters, kernel_size=kernel_size, groups=in_filters),
 				 nn.BatchNorm2d(out_filters),nn.LeakyReLU(0.2, inplace=True)]
 			return block
 
